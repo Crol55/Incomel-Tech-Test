da14b49267ea562ed108a04b8d3054f05e41a9d3
carlos <carlosorantes77@gmail.com>
2023-03-26 14:07:02 -0600

    Se agrego el DML y el DDL inicial para creacion de base de datos en MYSQL

---
+++ Base de datos/DDL_Script.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS INCOMEL;
+
+USE INCOMEL;
+
+CREATE TABLE USUARIO(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`nombre` varchar(50) NOT NULL,
+    `email` varchar(100) NOT NULL UNIQUE,
+    `fecha_nacimiento` DATE NOT NULL,
+    `password` varchar(50) NOT NULL,
+    `fecha_creacion` DATETIME NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE EMPLEADO(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`dpi` VARCHAR(15) NOT NULL UNIQUE,
+	`nombre_completo` VARCHAR(150) NOT NULL,
+	`cantidad_de_hijos` TINYINT UNSIGNED NOT NULL,
+	`salario_base` FLOAT NOT NULL,
+	`bono_decreto` FLOAT NOT NULL DEFAULT 250.00,
+	`deleted` TINYINT NOT NULL DEFAULT 0,
+	`fecha_creacion` DATETIME NOT NULL DEFAULT NOW(),
+    `USUARIO_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+  CONSTRAINT `fk_EMPLEADO_USUARIO` FOREIGN KEY (`USUARIO_id`) REFERENCES USUARIO(id)
+);
