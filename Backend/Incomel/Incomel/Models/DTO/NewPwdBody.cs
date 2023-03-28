06d77cad90e9e078b6f5ac98934b9d5cd48369ee
carlos <carlosorantes77@gmail.com>
2023-03-28 05:16:54 -0600

    se agrego un campo al modelo de base de datos, y otros DTO para comunicacion del backend con el frontend

---
+++ Backend/Incomel/Incomel/Models/DTO/NewPwdBody.cs
@@ -0,0 +1,9 @@
+﻿namespace Incomel.Models.DTO
+{
+    public class NewPwdBody
+    {
+        public string Email { get; set; }
+        public string Reset_password_code { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Backend/Incomel/Incomel/Models/DTO/ResetPwdBody.cs
@@ -0,0 +1,9 @@
+﻿namespace Incomel.Models.DTO
+{
+    public class ResetPwdBody
+    {
+        public string Email { get; set; }
+
+        public string Fecha_nacimiento { get; set; }
+    }
+}
